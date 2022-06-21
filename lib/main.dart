7a503cc4a22258de7c6a16a241819aee7f8830ea
Amyr Fezzeni <amyrfezzeni9@gmail.com>
2022-06-21 21:02:05 +0100

    21.6.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:taba3ni/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -19,7 +23,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const Scaffold(
+        backgroundColor: Color.fromARGB(255, 223, 223, 223),
+        body: SizedBox(
+          child: Center(
+            child: Text("Taba3ni"),
+          ),
+        ),
+      ),
     );
   }
 }
